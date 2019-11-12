2df12b52d2ca4f5ff37dead224adbd624fb6e7b7
Richard Nguyen <reechardc@gmail.com>
2019-11-11 16:59:22 -0800

    Bubble Sort Algorithm
    added bubble sort algorithm implementation and unit test

---
+++ Algorithms-DataStruct-Lib.Tests/BubbleSortTest.cs
@@ -0,0 +1,53 @@
+﻿
+using DataStructures_Algorithm;
+using NUnit.Framework;
+using System;
+
+namespace Algorithms_DataStruct_Lib.Tests
+{
+    [TestFixture]
+    public class BubbleSortTest
+    {
+        [Test]
+        public void BubbleSort_ValidInput_SortedInput()
+        {
+            RunTestForBubbleSort(BubbleSort.BubbleSortImplementation);
+        }
+
+        private int[][] SampleData()
+        {
+            int[][] sampleData = new int[9][];
+            sampleData[0] = new[] { 1 };
+            sampleData[1] = new[] { 2, 1 };
+            sampleData[2] = new[] { 2, 1, 3 };
+            sampleData[3] = new[] { 1, 1, 1 };
+            sampleData[4] = new[] { 2, -1, 3, 3 };
+            sampleData[5] = new[] { 4, -5, 3, 3 };
+            sampleData[6] = new[] { 0, -5, 3, 3 };
+            sampleData[7] = new[] { 0, -5, 3, 0 };
+            sampleData[8] = new[] { 3, 2, 5, 5, 1, 0, 7, 8 };
+
+            return sampleData;
+        }
+
+        private void RunTestForBubbleSort(Action<int[]> sort)
+        {
+            foreach (int[] sampleData in SampleData())
+            {
+                sort(sampleData);
+                CollectionAssert.IsOrdered(sampleData);
+                PrintOut(sampleData);
+            }
+        }
+
+        private void PrintOut(int[] array)
+        {
+            TestContext.WriteLine("-----TRACE-----\n");
+            foreach (int el in array)
+            {
+                TestContext.WriteLine(el + " ");
+            }
+            TestContext.WriteLine("\n------------------\n");
+        }
+    }
+}
---
+++ DataStructuresAlgorithm/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithm
+{
+    public class BubbleSort
+    {
+        /*
+         * Bubble Sort
+         * In-place algorithm - uses a small amount of extra memory
+         * Time Complexity - O(n^2)
+         * Degrades quickly
+         */
+
+        //Implementation
+        public static void BubbleSortImplementation(int[] array)
+        {
+            for(int partIndex = array.Length-1; partIndex > 0; partIndex--)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if(array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+        private static void Swap(int[] array, int i, int j)
+        {
+            if(i == j)
+            {
+                return;
+            }
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
